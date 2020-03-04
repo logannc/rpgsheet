252fa70babf7b44a02216b6d7851fc8065cc9a82
Logan Collins <logan@lcspace.net>
2020-03-03 19:46:57 -0700

    add support for proficiency bonus specific box

--- sapperapp/src/components/dnd5e/layout/LeftValueBox.svelte
+++ sapperapp/src/components/dnd5e/layout/LeftValueBox.svelte
@@ -1,6 +1,13 @@
 <script>
 export let box;
 export let text;
+export let container = 'inspiration_box';
+let container_image = '/images/dnd5e/standard/' + container + '.svg';
+let tweaked_left_pad = {
+    proficiency_bonus_box : "12%",
+    inspiration_box : "10%",
+}
+let left_pad = tweaked_left_pad[container] || "10%";
 </script>
 
 <style>
@@ -11,7 +18,6 @@ export let text;
         display: flex;
         align-items: center;
         /* TODO: add more box types again */
-        background-image: url('/images/dnd5e/standard/inspiration_box.svg');
         background-size: 80%;
         background-position: center;
         background-repeat: no-repeat;
@@ -19,7 +25,6 @@ export let text;
     }
     .box {
         min-width: 30px;
-        margin-left: 10%;
         text-align: center;
     }
     .textbox {
@@ -35,8 +40,8 @@ export let text;
 </style>
 
 <div class="negacontainer">
-    <div class="container">
-        <span class="box">{box}</span>
+    <div class="container" style="background-image: url('{container_image}');">
+        <span class="box" style="margin-left: {left_pad}">{box}</span>
         <div class="textbox">
             <span class="text">{text}</span>
         </div>
--- sapperapp/src/components/dnd5e/standard/ProficiencyBonus.svelte
+++ sapperapp/src/components/dnd5e/standard/ProficiencyBonus.svelte
@@ -3,4 +3,4 @@ import LeftValueBox from '../layout/LeftValueBox.svelte';
 export let box;
 </script>
 
-<LeftValueBox {box} text="Proficiency Bonus" />
+<LeftValueBox {box} text="Proficiency Bonus" container="proficiency_bonus_box" />
